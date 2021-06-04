ed007784035d8f02d780e8470f43bca618afceb0
Artem Litvinenko <70752711+art-litv@users.noreply.github.com>
2021-06-04 22:18:48 +0300

    debug turned off

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c5a_k@uohoeskg=$7!_yf+up#ks%m)oe62!jl@3(6=gds!v93k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
